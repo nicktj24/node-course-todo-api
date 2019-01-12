3201073a6876cf45fd91c324754f5d1e8f7e5c0e
Nikhil <nicktj24@gmail.com>
2019-01-12 23:27:00 +0530

    Small changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect( process.env.MONGODB_URI , {useNewUrlParser:true});
+mongoose.connect( process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp', {useNewUrlParser:true});
 
 module.exports = {
   // mongoose:mongoose Or if both varible name and resource name is same then we can write only resource name as

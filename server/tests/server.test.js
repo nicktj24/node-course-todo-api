f8b0e578e53797a09c9a25f05dce1136169807b6
Nikhil <nicktj24@gmail.com>
2019-01-12 20:14:38 +0530

    Add tests for route get('/todos/:id')

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,14 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./../db/mongoose'); //Reference to db/mongoose.js's exported mongoose object
 var {Todo} = require('./../models/todo'); // Reference to models/todo.js
 var {app} = require('./../server') // Reference to server/server.js's "app" variable
 
 var todos = [
-  {text:'Go to Gym'},
+  { _id : new ObjectID(),
+    text:'Go to Gym'},
   {text:'Go to Running'}
 ]
 
@@ -68,8 +70,35 @@ describe('GET /todos', () => {
     .expect(200)
     .expect((res) => {
       expect(res.body.todos.length).toBe(2);
-       expect()
   })
   .end(done);
   });
+});
+
+describe('GET todos/:id',() => {
+  it('Should return todo doc', (done) => {
+    request(app)
+    .get(`/todos/${todos[0]._id.toHexString()}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toBe(todos[0].text)
+    })
+    .end(done);
+  });
+
+ it('Should return 404 if todo not found ', (done) => {
+   let tid = new ObjectID().toHexString();
+   request(app)
+   .get(`/todos/${tid}}`)
+   .expect(404)
+   .end(done);
+ });
+
+ it('Should return 404 if id is invalid', (done) => {
+   request(app)
+   .get('/todos/12434432422')
+   .expect(404)
+   .end(done);
+ });
+
 });

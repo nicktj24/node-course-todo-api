61bfde8797fe0d5b81ef877bd2294658b1252303
Nikhil <nicktj24@gmail.com>
2019-01-13 16:11:22 +0530

    Add route PATCH /todos/:id for updating todo

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -64,6 +65,32 @@ app.delete('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send('ObjectID is invalid');
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+   Todo.findByIdAndUpdate(id, {$set : body},{new : true}).then((todo) => {
+     if(!todo){
+       return res.status(404).send('Not Found !');
+     }
+     res.send({todo});
+   }).catch((e) => {
+     res.status(400).send(e);
+   });
+
 });
 
 app.listen(PORT, () => {

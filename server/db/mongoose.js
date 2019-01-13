589d4bb4c749de46aec457968cb8ce21712f5b8f
Nikhil <nicktj24@gmail.com>
2019-01-13 23:04:06 +0530

    Add config file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoAppTest' , {useNewUrlParser:true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser:true});
 
 module.exports = {
   // mongoose:mongoose Or if both varible name and resource name is same then we can write only resource name as
   // below
   mongoose
 }
+
+// Solution But i don't understand why above line throwing a code
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoAppTest' , {useNewUrlParser:true});
